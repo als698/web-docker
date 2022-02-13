bad8b9f9b9045ae9b8bc06a246e569db4c65c5d9
als698 <no@mail.com>
2022-02-13 02:00:55 +0200

    persistance fix

--- config/mysql.sh
+++ config/mysql.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+set -eo pipefail
+shopt -s nullglob
+
+mkdir -p /db
 chmod 777 -R /db
 if [ ! -d "/run/mysqld" ]; then
     mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
+    chmod 777 /run/mysqld
+    mysql_installed=0
+else
+    rm -f /run/mysqld/msqld.sock
+    mysql_installed=1
 fi
+
 
 if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
     MYSQL_ROOT_PASSWORD="t0rpas"
@@ -33,13 +44,15 @@ fi
 if [ -d /db/data/mysql ]; then
     echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: MySQL directory already present, skipping creation"
 else
-    echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: creating database"
-    mysql_install_db --user=nobody > /dev/null
-    echo "USE mysql;" >> $tfile
-    echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> $tfile
-    echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '' WITH GRANT OPTION;" >> $tfile
-    echo "DROP DATABASE IF EXISTS test;" >> $tfile
-    echo "FLUSH PRIVILEGES;" >> $tfile
+  echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: creating database"
+  mysql_install_db > /dev/null
+  echo "USE mysql;" >> $tfile
+  echo "TRUNCATE mysql.user;" >> $tfile
+  echo "FLUSH PRIVILEGES;" >> $tfile
+  echo "CREATE USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> $tfile
+  echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" >> $tfile
+  echo "DROP DATABASE IF EXISTS test;" >> $tfile
+  echo "FLUSH PRIVILEGES;" >> $tfile
 fi
 
 if [ -d /db/data/${MYSQL_DATABASE} ]; then
@@ -50,15 +63,20 @@ else
 fi
 
 echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: creating $MYSQL_USER"
-# echo "CREATE USER \`$MYSQL_USER\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> $tfile
+echo "USE mysql;" >> $tfile
+echo "DROP USER IF EXISTS $MYSQL_USER;" >> $tfile
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
 echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: grant privileges $MYSQL_USER to $MYSQL_DATABASE"
-echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO \`$MYSQL_USER\`@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
-echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO \`$MYSQL_USER\`@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%';" >> $tfile
 
 /usr/bin/mysqld --skip-networking &
 pid="$!"
 
-mysql=( mysql --protocol=socket --socket=/run/mysqld/mysqld.sock )
+if [ "$mysql_installed" = "0" ]; then
+  mysql=( mysql --protocol=socket -uroot -hlocalhost --socket=/run/mysqld/mysqld.sock )
+else
+  mysql=( mysql --protocol=socket -uroot -p$MYSQL_ROOT_PASSWORD -hlocalhost --socket=/run/mysqld/mysqld.sock )
+fi
 
 for i in {30..0}; do
     echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: MySQL init process in progress..."
@@ -84,6 +102,8 @@ fi
 
 "${mysql[@]}" < $tfile
 
+rm -f $tfile
+
 if ! kill -s TERM "$pid" || ! wait "$pid"; then
     echo >&2 $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: MySQL init process failed."
     exit 1
@@ -91,7 +111,18 @@ else
     echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: Database initiated [2/2]"
 fi
 
-rm -f $tfile
+if [ ! -f "/var/www/pma/index.php" ]; then
+  echo $(date '+%Y-%m-%d %H:%M:%S') "web [info]: Installing phpMyAdmin"
+  unzip /pma.zip -d /var/www/
+  echo $(date '+%Y-%m-%d %H:%M:%S') "web [info]: phpMyAdmin installed [OK]"
+fi
+
+if [ ! -f "/var/www/html/index.php" ]; then
+    cp /index.php /var/www/html/
+fi
+chmod -R 777 /var/www/
+chmod -R 644 /var/www/*/config*.php
+chown -R nginx.nginx /var/www
 
 echo $(date '+%Y-%m-%d %H:%M:%S') "STARTING DATABASE"
 
